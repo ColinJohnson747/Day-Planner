e21fd503535552993cd007045de2cd729027912d
Colin Johnson <blisk76@gmail.com>
2020-03-22 17:08:20 -0400

    finished save function, need to do CSS portion

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
   var m = moment();
 
   // variables for on save button click
-  var words
+  var words;
   var hourInfo;
   console.log(m.format("dddd, MMMM Do"));
   $("#currentDay").text(m.format("dddd, MMMM Do"));
+
+  renderText();
 
   //Save function by listening for clicks on the save button
   $(".save").click(function() {
@@ -21,18 +23,48 @@ $(document).ready(function() {
     console.log(hourInfo);
     localStorage.setItem(hourInfo, JSON.stringify(words));
 
-
+    renderText();
   });
 
-  function changeColor(){
-      var realTime = moment().hours();
-      console.log("current time "+ realTime);
+  //function to change CSS to show present, past, future
 
-    $(".imput").each(function(){
-      var timeTest = parseInt($(this).attr("id"));
-      
-    })
-    $
+  function renderText() {
+    var saveWords9am = JSON.parse(localStorage.getItem("9am"));
+    $("#9").val("");
+    $("#9").val(saveWords9am);
+
+    var saveWords10am = JSON.parse(localStorage.getItem("10am"));
+    $("#10").val("");
+    $("#10").val(saveWords10am);
+
+    var saveWords11am = JSON.parse(localStorage.getItem("11am"));
+    $("#11").val("");
+    $("#11").val(saveWords11am);
+
+    var saveWords12pm = JSON.parse(localStorage.getItem("12pm"));
+    $("#12").val("");
+    $("#12").val(saveWords12pm);
+
+    var saveWords1pm = JSON.parse(localStorage.getItem("1pm"));
+    $("#13").val("");
+    $("#13").val(saveWords1pm);
+
+    var saveWords2pm = JSON.parse(localStorage.getItem("2pm"));
+    $("#14").val("");
+    $("#14").val(saveWords2pm);
+
+    var saveWords3pm = JSON.parse(localStorage.getItem("3pm"));
+    $("#15").val("");
+    $("#15").val(saveWords3pm);
+
+    var saveWords4pm = JSON.parse(localStorage.getItem("4pm"));
+    $("#16").val("");
+    $("#16").val(saveWords4pm);
+
+    var saveWords5pm = JSON.parse(localStorage.getItem("5pm"));
+    $("#17").val("");
+    $("#17").val(saveWords5pm);
   }
-  
+
+  renderText();
 });
