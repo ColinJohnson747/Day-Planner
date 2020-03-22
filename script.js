5a7c8df233763120b55c1eddb0597bd54cdd9788
Colin Johnson <blisk76@gmail.com>
2020-03-22 14:56:06 -0400

    Changed HTML to have new ID's, added javascript to run date fetch

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  //var = function to call current moment/time
+  var m = moment();
+
+  // variables for on save button click
+  var words;
+  var hourInfo;
+  console.log(m.format("dddd, MMMM Do"));
+
+  $("#currentDay").text(m.format("dddd, MMMM Do"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+});
