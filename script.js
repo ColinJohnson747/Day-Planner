caa9953e76f233185758bd6a5c480dea48c63332
Colin Johnson <blisk76@gmail.com>
2020-03-25 18:27:02 -0400

    added function to create rows, must write time function

--- script.js
+++ script.js
@@ -18,10 +18,30 @@ $(document).ready(function() {
     //to make the hours row, the var of "i" will be the hour appended.Also learned that you can use ${} to insert data into strings when the string starts with `. Helpful!
     var row = $(`<div data-time=${i} id='${i}' class='row'>`);
     //to make our Hour column
-    var column1 = $('<div class="col-sm-1"> <p class="hour">' + formatAMPM(i) + '</p>')
+    var column1 = $('<p class="hour">' + formatTime(i) + "</p>");
     //to make our fillable column
-    var column2 = $("<div class='col-sm-8 past'> <textarea id='text'" + i)
+    var column2 = $(
+      `<textarea id=text${i} class="entry col-sm-9 past"></textarea>`
+    );
+    //to make our savebutton column
+    var column3 = $(
+      `<button class="btn btn-primary btn-lg col-sm-1 id=${i}></button`
+    );
+
+    //now we just append these columns to the above row
+    row.append(column1);
+    row.append(column2);
+    row.append(column3);
+
+    //and then we append those rows to the container
+    $(".container").append(row);
+
+    //and grab any saved info for i
+    getLocalStorage(i);
   }
+
+  
+ 
 
   //Save function by listening for clicks on the save button
   $(".save").click(function() {
@@ -40,46 +60,7 @@ $(document).ready(function() {
     changeColor();
   });
 
-  function renderText() {
-    var saveWords9am = JSON.parse(localStorage.getItem("9am"));
-    $("#9").val("");
-    $("#9").val(saveWords9am);
-
-    var saveWords10am = JSON.parse(localStorage.getItem("10am"));
-    $("#10").val("");
-    $("#10").val(saveWords10am);
-
-    var saveWords11am = JSON.parse(localStorage.getItem("11am"));
-    $("#11").val("");
-    $("#11").val(saveWords11am);
-
-    var saveWords12pm = JSON.parse(localStorage.getItem("12pm"));
-    $("#12").val("");
-    $("#12").val(saveWords12pm);
-
-    var saveWords1pm = JSON.parse(localStorage.getItem("1pm"));
-    $("#13").val("");
-    $("#13").val(saveWords1pm);
-
-    var saveWords2pm = JSON.parse(localStorage.getItem("2pm"));
-    $("#14").val("");
-    $("#14").val(saveWords2pm);
-
-    var saveWords3pm = JSON.parse(localStorage.getItem("3pm"));
-    $("#15").val("");
-    $("#15").val(saveWords3pm);
-
-    var saveWords4pm = JSON.parse(localStorage.getItem("4pm"));
-    $("#16").val("");
-    $("#16").val(saveWords4pm);
-
-    var saveWords5pm = JSON.parse(localStorage.getItem("5pm"));
-    $("#17").val("");
-    $("#17").val(saveWords5pm);
-  }
-
-  renderText();
-});
+  
 
 function changeColor() {
   var currentTime = moment().hours();
