8904b55d93ba257879f3b996cee8de7c343d6a8a
Colin Johnson <blisk76@gmail.com>
2020-03-26 00:19:13 -0400

    divs right themself, redid saves and color change around it

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ $(document).ready(function() {
   var words;
   var hourInfo;
 
+  //grabs stored info
+  function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+      $(`#text${key}`).text(value);
+    }
+  }
+
   //Appending Date to Page
   console.log(m.format("dddd, MMMM Do"));
   $("#currentDay").text(m.format("dddd, MMMM Do"));
@@ -18,16 +26,19 @@ $(document).ready(function() {
     //to make the hours row, the var of "i" will be the hour appended.Also learned that you can use ${} to insert data into strings when the string starts with `. Helpful!
     var row = $(`<div data-time=${i} id='${i}' class='row'>`);
     //to make our Hour column
-    var column1 = $('<p class="hour">' + formatTime(i) + "</p>");
+    var column1 = $('<p class="hour col-1">' + formatTime(i) + "</p>");
     //to make our fillable column
     var column2 = $(
-      `<textarea id=text${i} class="entry col-sm-9 past"></textarea>`
+      $(
+        `<textarea id=text${i} class="description col-10 past" placeholder="Add your event here..."></textarea>`
+      )
     );
     //to make our savebutton column
     var column3 = $(
-      `<button class="btn btn-primary btn-lg col-sm-1 id=${i}></button`
+      `<button class="btn btn-primary btn-lg col-1 saveBtn" id=${i}>Save</button>`
     );
 
+    var column4 = $("</hr>");
     //now we just append these columns to the above row
     row.append(column1);
     row.append(column2);
@@ -36,36 +47,42 @@ $(document).ready(function() {
     //and then we append those rows to the container
     $(".container").append(row);
 
+    $(".row").append(column4);
+
     //and grab any saved info for i
     getLocalStorage(i);
   }
 
-  
- 
+  //function for converting time from 24 hr to 12hr.
+  function formatTime(hours) {
+    var ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    return hours + ampm;
+  }
+  formatTime();
 
   //Save function by listening for clicks on the save button
-  $(".save").click(function() {
-    var words = $(this)
-      .siblings(".input")
+
+  function changeColor() {
+    var nowTime = new Date().getHours();
+    for (var i = 9; i < 18; i++) {
+      console.log(nowTime, $(`#${i}`).data("time"));
+      if ($(`#${i}`).data("time") == currentTime) {
+        $(`#text${i}`).addClass("present");
+      } else if (currentTime < $(`#${i}`).data("time")) {
+        $(`#text${i}`).addClass("future");
+      }
+    }
+  }
+  var saveBtn = $(".saveBtn");
+  saveBtn.on("click", function() {
+    let eventId = $(this).attr("id");
+    let eventText = $(this)
+      .parent()
+      .siblings()
+      .children(".description")
       .val();
-    console.log(words);
-
-    hourInfo = $(this)
-      .siblings(".hour")
-      .text();
-    console.log(hourInfo);
-    localStorage.setItem(hourInfo, JSON.stringify(words));
-
-    renderText();
-    changeColor();
+    localStorage.setItem(eventId, eventText);
   });
-
-  
-
-function changeColor() {
-  var currentTime = moment().hours();
-  var x = $(".hour");
-  x.innerHTML;
-}
-
-changeColor();
+});
